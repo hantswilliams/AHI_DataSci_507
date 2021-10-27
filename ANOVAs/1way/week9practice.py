b6985d6eaee9dc4a2e9cb16b95e0048fb3e03e12
hants <hantsawilliams@gmail.com>
2021-10-27 16:29:58 -0700

    added in data load steps

--- ANOVAs/1way/week9practice.py
+++ ANOVAs/1way/week9practice.py
@@ -6,3 +6,13 @@ Created on Wed Oct 27 16:27:46 2021
 @author: hantswilliams
 """
 
+
+
+import pandas as pd
+
+diabetes = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/AHI_DataSci_507/main/Datasets/Diabetes/DB1_Diabetes/diabetic_data.csv')
+
+diabetes_small = diabetes.sample(100)
+
+### generate list of var names 
+list(diabetes_small)
