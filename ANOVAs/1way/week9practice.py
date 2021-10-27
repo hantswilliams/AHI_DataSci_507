466e02ed8c022fa3bb40dac8710fff544d1e5102
hants <hantsawilliams@gmail.com>
2021-10-27 16:57:22 -0700

    added more details

--- ANOVAs/1way/week9practice.py
+++ ANOVAs/1way/week9practice.py
@@ -35,9 +35,47 @@ diabetes['race'] = diabetes['race'].replace(np.NaN, 'Other')
 diabetes.race.value_counts()
 diabetes['race'].value_counts() 
 
-# 1 factor of AGE 
+# 1 factor of AGE we have 10 levels 
 diabetes.age.value_counts() 
 diabetes['age'].value_counts() 
+
+# 1 factor of PAYER_CODE we have 17 levels 
+diabetes.payer_code.value_counts()
+len(diabetes.payer_code.value_counts())
+
+# 1 factor of medical_specialty we have 72 levels 
+diabetes.medical_specialty.value_counts()
+len(diabetes.medical_specialty.value_counts())
+
+specialtycounts = pd.DataFrame(diabetes.medical_specialty.value_counts())      
+specialtycounts = specialtycounts.reset_index()
+
+
+#### Continuous values: 
+    
+timeinhospital = diabetes['time_in_hospital']
+labprocedures = diabetes['num_lab_procedures'] 
+
+
+
+
+# 1 way anova 
+# 1 DV 
+# 1 IV - 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
