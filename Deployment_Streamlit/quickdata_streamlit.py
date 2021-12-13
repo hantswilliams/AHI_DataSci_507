4d90249066f40a15dd16c86965047e8db61dfb68
hants <hantsawilliams@gmail.com>
2021-12-13 16:41:46 -0500

    removed plotly defalut

--- Deployment_Streamlit/quickdata_streamlit.py
+++ Deployment_Streamlit/quickdata_streamlit.py
@@ -14,9 +14,7 @@ import streamlit as st
 
 import pandas as pd
 import numpy as np
-import plotly.express as px
-from plotly.subplots import make_subplots
-import plotly.graph_objects as go
+import plotly.figure_factory as ff
 import time
 
 
